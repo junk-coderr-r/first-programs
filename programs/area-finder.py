03c355a5ce5eda6402cc7d8b72f9236acb32d067
junk-coderr-r <freed270283@waukeeschools.org>
2024-12-11 11:37:39 -0600

    Create area-finder.py
    god knows how many bugs are in here

---
+++ programs/area-finder.py
@@ -0,0 +1,35 @@
+# for finding area, was in my math class and bored
+x = int(input("What shape is this? Parallelogram(1), Rhombus/kite(2), or a triangle(3)?"))
+
+if x==1:
+    def parallelogram():
+        base = float(input("What is the base measurement?"))
+        height = float(input("What is the height?"))
+        area = base * height
+        print(area)
+        exit()
+    parallelogram()
+
+if x==2:
+    def Rhombus():
+        distance_1 = float(input("What is the first distance measurement?"))
+        distance_2 = float(input("What is the second distance measurement?"))
+        mult = int(input("Does the distance need to be multiplied by 2, Yes(1) or No(2)"))
+        if mult==1:
+            distance_1 *= 2
+            distance_2 *= 2
+        multiplied = distance_1*distance_2
+        multiplied/=2
+        print(multiplied)
+        exit()
+    Rhombus()
+
+if x==3:
+    def parallelogram():
+        base = float(input("What is the base measurement?"))
+        height = float(input("What is the height?"))
+        area = base * height
+        area/=2
+        print(area)
+        exit()
+    parallelogram()
