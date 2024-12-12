08e256ad6b0d994729140783785f030c8d571b47
junk-coderr-r <freed270283@waukeeschools.org>
2024-12-12 09:56:57 -0600

    added saving

--- programs/number-to-binary.py
+++ programs/number-to-binary.py
@@ -1,16 +1,29 @@
 #turns a number to binary, just breaks if you put in a negative number because im too lazy to fix it
+saved = []
 
-x = int(input("Give a number you'd like to turn to binary "))
+def binaryconvert():
+    initial = int(input("Give a number you'd like to turn to binary "))
 
-binary = []
+    x = initial
 
-while x > 0:
-    rem = x % 2
-    binary.append(rem)
-    x //= 2
-    print(x, rem)
+    binary = []
 
-backwards = binary[::-1]
-backwards = ''.join(str(x) for x in backwards)
-print(backwards)
-exit()
+    while x > 0:
+        rem = x % 2
+        binary.append(rem)
+        x //= 2
+        print(x, rem)
+
+    backwards = binary[::-1]
+    backwards = ''.join(str(x) for x in backwards)
+    print(backwards)
+    save = int(input("Would you like to save this input? 1 to save, 2 to close."))
+
+    if save == 1:
+        finished = str(initial) + ' = ' + backwards
+        saved.append(finished)
+        print(saved)
+        binaryconvert()
+    else:
+        exit()
+binaryconvert()
